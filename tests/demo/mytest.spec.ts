ef55fdfdbaac90fe9ef586dbc17e5e4f76cc9a31
Brijendra Singh <brijj2006@gmail.com>
2026-04-18 01:46:28 +0800

    added few tests

--- tests/demo/mytest.spec.ts
+++ tests/demo/mytest.spec.ts
@@ -1,25 +1,32 @@
 import { test, expect } from "@playwright/test";
 
-test("should have the correct title and header", async ({ page }) => {
-  await page.goto("https://katalon-demo-cura.herokuapp.com/");
-  await expect(page).toHaveTitle("CURA Healthcare Service");
-  await expect(page.locator("//h1")).toHaveText("CURA Healthcare Service");
-});
-
-test("smoke test for application", { tag: "@smoke" }, async ({ page }) => {
-  await page.goto("https://katalon-demo-cura.herokuapp.com/");
-  await expect(page).toHaveTitle("CURA Healthcare Service");
-  await expect(page.locator("//h3")).toHaveText("We Care About Your Health");
-});
-
-test(
-  "regression test for the application",
-  { tag: "@regression" },
-  async ({ page }) => {
+test.describe("Login feature", () => {
+  test.beforeEach("Go to the login page", async ({ page }) => {
     await page.goto("https://katalon-demo-cura.herokuapp.com/");
+    await expect(page).toHaveTitle("CURA Healthcare Service");
+  });
+
+  test("smoke test", { tag: "@smoke" }, async ({ page }) => {
+    await expect(page.locator("//h3")).toHaveText("We Care About Your Health");
+  });
+
+  test("regression test", { tag: "@regression" }, async ({ page }) => {
+    // await page.goto("https://katalon-demo-cura.herokuapp.com/");
     await page.locator("#btn-make-appointment").click();
     await expect(page.locator(".lead")).toHaveText(
       "Please login to make appointment.",
     );
-  },
-);
+  });
+
+  test("make apppointment", { tag: "@regression" }, async ({ page }) => {
+    // await page.goto("https://katalon-demo-cura.herokuapp.com/");
+    await page.getByRole("link", { name: "Make Appointment" }).click();
+    await expect(page.getByText("Please login to make")).toBeVisible();
+    await page.getByLabel("Username").click();
+    await page.getByLabel("Username").fill("John Doe");
+    await page.getByLabel("Password").click();
+    await page.getByLabel("Password").fill("ThisIsNotAPassword");
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(page.locator("h2")).toContainText("Make Appointment");
+  });
+});
