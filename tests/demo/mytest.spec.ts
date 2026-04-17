c8d1e50f11e46304abe25a8a5d53b826ef01aa6d
Brijendra Singh <brijj2006@gmail.com>
2026-04-18 00:58:27 +0800

    added few tests

--- tests/demo/mytest.spec.ts
+++ tests/demo/mytest.spec.ts
@@ -12,10 +12,14 @@ test("smoke test for application", { tag: "@smoke" }, async ({ page }) => {
   await expect(page.locator("//h3")).toHaveText("We Care About Your Health");
 });
 
-test("", { tag: "@regression" }, async ({ page }) => {
-  await page.goto("https://katalon-demo-cura.herokuapp.com/");
-  await page.locator("#btn-make-appointment").click();
-  await expect(page.locator(".lead")).toHaveText(
-    "Please login to make appointment.",
-  );
-});
+test(
+  "regression test for the application",
+  { tag: "@regression" },
+  async ({ page }) => {
+    await page.goto("https://katalon-demo-cura.herokuapp.com/");
+    await page.locator("#btn-make-appointment").click();
+    await expect(page.locator(".lead")).toHaveText(
+      "Please login to make appointment.",
+    );
+  },
+);
