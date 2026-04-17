67c10167156d37315749d061aecfd0348b2b42bd
Brijendra Singh <brijj2006@gmail.com>
2026-04-18 00:47:54 +0800

    added few tests

--- tests/demo/mytest.spec.ts
+++ tests/demo/mytest.spec.ts
@@ -6,11 +6,16 @@ test("should have the correct title and header", async ({ page }) => {
   await expect(page.locator("//h1")).toHaveText("CURA Healthcare Service");
 });
 
-test("should do something", { tag: "@smoke" }, async ({ page }, testInfo) => {
-  await expect(page).toHaveURL("");
-  await expect(page.locator("")).toBeVisible();
+test("smoke test for application", { tag: "@smoke" }, async ({ page }) => {
+  await page.goto("https://katalon-demo-cura.herokuapp.com/");
+  await expect(page).toHaveTitle("CURA Healthcare Service");
+  await expect(page.locator("//h3")).toHaveText("We Care About Your Health");
 });
 
 test("", { tag: "@regression" }, async ({ page }) => {
-  await page.locator("").setInputFiles("");
+  await page.goto("https://katalon-demo-cura.herokuapp.com/");
+  await page.locator("#btn-make-appointment").click();
+  await expect(page.locator(".lead")).toHaveText(
+    "Please login to make appointment.",
+  );
 });
