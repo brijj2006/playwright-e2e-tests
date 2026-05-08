55db162a9e231e4b18df3fb91e62748343e0a089
Brijendra Singh <brijj2006@gmail.com>
2026-05-08 17:54:59 +0800

    added mocked tests

--- tests/demo/api.intercept.mock.spec.ts
+++ tests/demo/api.intercept.mock.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import tags from "../../mocks/tags.json" with { type: "json" };
+import articles from "../../mocks/articles.json" with { type: "json" };
 
 test("API Intercept Mock Test", { tag: "@mock" }, async ({ page }) => {
   // intercept API request
@@ -14,4 +15,22 @@ test("API Intercept Mock Test", { tag: "@mock" }, async ({ page }) => {
 
   await page.goto("https://conduit.bondaracademy.com/");
   await expect(page.locator(".tag-list")).toContainText("Hello World");
+});
+
+test("API Mock Test", { tag: "@mock" }, async ({ page }) => {
+  await page.route("**/*/api/articles?limit=10&offset=0", async (route) => {
+    if (route.request().method().includes("GET")) {
+      await route.fulfill({
+        body: JSON.stringify(articles),
+      });
+    } else {
+      await route.continue();
+    }
+  });
+
+  await page.goto("https://conduit.bondaracademy.com/");
+  await expect(page.locator(".preview-link")).toHaveCount(1);
+  await expect(page.locator(".preview-link")).toContainText(
+    "Mocking API test with Bridge",
+  );
 });
